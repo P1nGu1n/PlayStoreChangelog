b17ea29664c3cdd1a8782e85d9ea735518f97898
P1nGu1n <p1ngu1n@outlook.com>
2014-10-30 00:22:17 +0100

    Initial commit

---
+++ PlayStoreChangelog/src/main/java/com/p1ngu1n/playstorechangelog/PlayStoreChangelog.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2014  P1nGu1n
+ *
+ * This file is part of Play Store Changelog.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.p1ngu1n.playstorechangelog;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * The class to be loaded by Xposed.
+ */
+public class PlayStoreChangelog implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        if (!loadPackageParam.packageName.equals("com.android.vending"))
+            return;
+
+        /*
+         * DetailsTextBlock is the block containing the "What's New" heading and the changelog.
+         * The maximum number of lines of the changelog gets set to the number passed as a parameter to the 'bind' method.
+         * This mod changes this parameter to the maximum integer value, so the changelog will always be fully shown.
+         */
+        Class<?> detailsTextBlockClass = XposedHelpers.findClass("com.google.android.finsky.layout.DetailsTextBlock", loadPackageParam.classLoader);
+        XposedHelpers.findAndHookMethod(detailsTextBlockClass, "bind", CharSequence.class, CharSequence.class, int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                // 0 = What's new text, 1 = changelog, 2 = maximum changelog lines
+                param.args[2] = Integer.MAX_VALUE;
+            }
+        });
+    }
+}
